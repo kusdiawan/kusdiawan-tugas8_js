99401939b49ff7874c314f386dd2f406e51522fb
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-09-18 11:24:20 +0700

    Add files via upload

---
+++ tugas8.js
@@ -0,0 +1,9 @@
+var dataPegawai = {nama: "Wawan Kusdiawan", alamat: "Karawang", noHP: "0899020788", usia : 41}
+
+ console.log("Nama : ", dataPegawai.nama);
+ console.log("Alamat : ", dataPegawai.alamat);
+ console.log("No HP : ", dataPegawai.noHP);
+ console.log("Usia : ", dataPegawai.usia);
+
+ 
+ 
