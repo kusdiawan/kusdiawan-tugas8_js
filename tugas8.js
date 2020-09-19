38708bee3c220ca40d4d298cdf608e1df18bfc80
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-09-19 13:26:46 +0700

    Update tugas8.js

--- tugas8.js
+++ tugas8.js
@@ -1,5 +1,13 @@
-var dataPegawai = {nama: "Wawan Kusdiawan", alamat: "Karawang", noHP: "0899020788", usia : 41}
+var dataPegawai = {nama: "Wawan Kusdiawan", 
+                   alamat: "Karawang", 
+                   noHP: "0899020788", 
+                   usia : 41
+                  }
 
+ //menampilkan langsung seluruh isi object
+ console.log(dataPegawai);
+
+//menampilkan per item data isi object
  console.log("Nama : ", dataPegawai.nama);
  console.log("Alamat : ", dataPegawai.alamat);
  console.log("No HP : ", dataPegawai.noHP);
